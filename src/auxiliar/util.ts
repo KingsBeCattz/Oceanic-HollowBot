af8b3f1ffc84275de038a656e51f3ac922be89c0
KingBeCats <=>
2024-08-15 13:45:35 -0700

    The function to badges was created and a command

--- src/auxiliar/util.ts
+++ src/auxiliar/util.ts
@@ -4,6 +4,24 @@ import { CommandManager } from './command.manager';
 import { EventManager } from './event.manager';
 import * as log from './logger';
 
+const badgets = {
+	STAFF: 1n << 0n,
+	PARTNER: 1n << 1n,
+	HYPESQUAD: 1n << 2n,
+	BUG_HUNTER_LEVEL_1: 1n << 3n,
+	HYPESQUAD_ONLINE_HOUSE_1: 1n << 6n,
+	HYPESQUAD_ONLINE_HOUSE_2: 1n << 7n,
+	HYPESQUAD_ONLINE_HOUSE_3: 1n << 8n,
+	PREMIUM_EARLY_SUPPORTER: 1n << 9n,
+	TEAM_PSEUDO_USER: 1n << 10n,
+	BUG_HUNTER_LEVEL_2: 1n << 14n,
+	VERIFIED_BOT: 1n << 16n,
+	VERIFIED_DEVELOPER: 1n << 17n,
+	CERTIFIED_MODERATOR: 1n << 18n,
+	BOT_HTTP_INTERACTIONS: 1n << 19n,
+	ACTIVE_DEVELOPER: 1n << 22n
+};
+
 export class Util {
 	commands: CommandManager;
 	events: EventManager;
@@ -103,5 +121,9 @@ export class Util {
 			case 'number':
 				return String(value).replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 		}
+	}
+
+	public badgets(flags: bigint): string[] {
+		return Object.keys(badgets).filter((k) => (flags & badgets[k]) !== 0n);
 	}
 }
