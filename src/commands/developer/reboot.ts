7f400db4842c640e622540e31acdee1fe674250e
KingBeCats <=>
2024-08-14 23:35:48 -0700

    Addition of command reboot system

---
+++ src/commands/developer/reboot.ts
@@ -0,0 +1,36 @@
+import { Command, CommandTypes } from 'src/builders/command.builder';
+
+export default new Command(
+	{
+		name: 'reboot',
+		description: 'Restart the whole bot process',
+		type: CommandTypes.Developer,
+		nsfw: false
+	},
+	[
+		async (ctx) =>
+			(
+				(await ctx.db.get('users', `${ctx.user.id}.permissions`)) as string[]
+			)?.includes('developer') ?? false
+	],
+	[],
+	async (ctx) => {
+		ctx.send({
+			content:
+				'Restarting...\n-# When I start back I will send notice on this same channel.'
+		});
+
+		ctx.db.set('client', 'notify.reboot', ctx.channel?.id ?? ctx.user.id);
+
+		const child = Bun.spawn({
+			cmd: process.argv,
+			detached: true,
+			stdout: 'pipe',
+			stderr: 'pipe'
+		});
+
+		child.unref();
+
+		process.exit(0);
+	}
+);
--- src/events/ready.ts
+++ src/events/ready.ts
@@ -17,5 +17,13 @@ export default new Event('ready', async () => {
 
 	setInterval(reloademojis, 30 * 60000);
 
+	if (await db.exists('client', 'notify.reboot')) {
+		client.rest.channels.createMessage(
+			(await db.get('client', 'notify.reboot')) as string,
+			{ content: 'Reboot completed!' }
+		);
+		await db.delete('client', 'notify.reboot');
+	}
+
 	log.info(`Logged as ${client.user.tag}`, 'CONNECTION');
 });
