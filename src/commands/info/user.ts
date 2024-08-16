24564aa3a56aa9140770af3aa14fab43ea38fafa
KingBeCats <=>
2024-08-15 23:18:09 -0700

    Number.parseInt instead parseInt global

--- src/commands/info/user.ts
+++ src/commands/info/user.ts
@@ -23,53 +23,68 @@ export default new Command(
 	async (ctx) => {
 		const member = ctx.get(
 			'user',
-			await ctx.util.findMember((ctx.guild as Guild).id, ctx.args?.shift() ?? ctx.user.id)
-        ) as Member;
-        
-        const timestamp = {
-            created: Number((member.user.createdAt.valueOf() / 1000).toFixed()),
-            joined: Number(((member.joinedAt ?? member.user.createdAt)?.valueOf() /1000).toFixed())
-        }
+			await ctx.util.findMember(
+				(ctx.guild as Guild).id,
+				ctx.args?.shift() ?? ctx.user.id
+			)
+		) as Member;
 
-		const data = (await (
-			await fetch(`https://discord-arts.asure.dev/v1/user/${member.id}`)
-		)
-			.json()
-			.catch((_) => ({}))).data;
+		const timestamp = {
+			created: Number((member.user.createdAt.valueOf() / 1000).toFixed()),
+			joined: Number(
+				((member.joinedAt ?? member.user.createdAt)?.valueOf() / 1000).toFixed()
+			)
+		};
+
+		const data = (
+			await (
+				await fetch(`https://discord-arts.asure.dev/v1/user/${member.id}`)
+			)
+				.json()
+				.catch((_) => ({}))
+		).data;
 
 		const photo = data.assets.avatarURL ?? data.assets.defaultAvatarURL;
 
 		ctx.send({
 			embeds: [
 				{
-					title: member.user.globalName ?? `${member.user.username}#${member.user.discriminator}`,
-					color: data.decoration.profileColors ? parseInt(data.decoration.profileColors[1].slice(1), 16) : ctx.util.random(16777215),
+					title:
+						member.user.globalName ??
+						`${member.user.username}#${member.user.discriminator}`,
+					color: data.decoration.profileColors
+						? Number.parseInt(data.decoration.profileColors[1].slice(1), 16)
+						: ctx.util.random(16777215),
 					image: {
 						url: `attachment://${member.id}.card.png`
 					},
 					thumbnail: {
 						url: `attachment://${member.id}.icon.png`
-                    },
-                    description: `- **Name**: ${member.user.globalName ?? member.user.username}\n - **Nick**: ${member.nick ?? 'Don\'t have a nickname'}\n- **User**: ${member.user.tag} (<@${member.id}>)\n- **Creation date**: <t:${timestamp.created}:d> <t:${timestamp.created}:T>\n${timestamp.joined ? `- **Joined date**: <t:${timestamp.joined}:d> <t:${timestamp.joined}:T>` : ''}`,
-                    fields: [
-                        {
-                            name: 'Medals',
-                            value: `Don't have any medal!`
-                        },
-                        {
-                            name: 'Roles',
-                            value: member.roles.length ? member.roles.map(r => `<@&${r}>`).format() : "It has no roles!"
-                        }
-                    ]
+					},
+					description: `- **Name**: ${member.user.globalName ?? member.user.username}\n - **Nick**: ${member.nick ?? "Don't have a nickname"}\n- **User**: ${member.user.tag} (<@${member.id}>)\n- **Creation date**: <t:${timestamp.created}:d> <t:${timestamp.created}:T>\n${timestamp.joined ? `- **Joined date**: <t:${timestamp.joined}:d> <t:${timestamp.joined}:T>` : ''}`,
+					fields: [
+						{
+							name: 'Medals',
+							value: `Don't have any medal!`
+						},
+						{
+							name: 'Roles',
+							value: member.roles.length
+								? member.roles.map((r) => `<@&${r}>`).format()
+								: 'It has no roles!'
+						}
+					]
 				}
 			],
 			files: [
 				{
 					name: `${member.id}.card.png`,
-                    contents: await profileImage(member.id, {
-                        badgesFrame: true,
-                        presenceStatus: member.presence?.clientStatus.mobile ? 'phone' : member.presence?.status
-                    })
+					contents: await profileImage(member.id, {
+						badgesFrame: true,
+						presenceStatus: member.presence?.clientStatus.mobile
+							? 'phone'
+							: member.presence?.status
+					})
 				},
 				{
 					name: `${member.id}.icon.png`,
