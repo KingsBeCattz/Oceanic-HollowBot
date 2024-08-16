63b52fd3b0de7448f58d4dd4ba4829129c6b5a28
KingBeCats <=>
2024-08-15 22:30:31 -0700

    Now the roles are well displayed!

--- src/commands/info/user.ts
+++ src/commands/info/user.ts
@@ -32,7 +32,7 @@ export default new Command(
         }
 
 		const data = (await (
-			await fetch(`https://discord-arts.asure.dev/v1/user/${member.user.id}`)
+			await fetch(`https://discord-arts.asure.dev/v1/user/${member.id}`)
 		)
 			.json()
 			.catch((_) => ({}))).data;
@@ -45,12 +45,12 @@ export default new Command(
 					title: member.user.globalName ?? `${member.user.username}#${member.user.discriminator}`,
 					color: data.decoration.profileColors ? parseInt(data.decoration.profileColors[1].slice(1), 16) : ctx.util.random(16777215),
 					image: {
-						url: `attachment://${member.user.id}.card.png`
+						url: `attachment://${member.id}.card.png`
 					},
 					thumbnail: {
-						url: `attachment://${member.user.id}.icon.png`
+						url: `attachment://${member.id}.icon.png`
                     },
-                    description: `- **Name**: ${member.user.globalName}\n - **Nick**: ${member.nick ?? 'Don\'t have a nickname'}\n- **User**: ${member.user.tag}\n- **Creation date**: <t:${timestamp.created}:d> <t:${timestamp.created}:T>\n${timestamp.joined ? `- **Joined date**: <t:${timestamp.joined}:d> <t:${timestamp.joined}:T>` : ''}`,
+                    description: `- **Name**: ${member.user.globalName}\n - **Nick**: ${member.nick ?? 'Don\'t have a nickname'}\n- **User**: ${member.user.tag} (<@${member.id}>)\n- **Creation date**: <t:${timestamp.created}:d> <t:${timestamp.created}:T>\n${timestamp.joined ? `- **Joined date**: <t:${timestamp.joined}:d> <t:${timestamp.joined}:T>` : ''}`,
                     fields: [
                         {
                             name: 'Medals',
@@ -58,18 +58,18 @@ export default new Command(
                         },
                         {
                             name: 'Roles',
-                            value: member.roles.format()
+                            value: member.roles.length ? member.roles.map(r => `<@&${r}>`).format() : "It has no roles!"
                         }
                     ]
 				}
 			],
 			files: [
 				{
-					name: `${member.user.id}.card.png`,
-					contents: await profileImage(member.user.id)
+					name: `${member.id}.card.png`,
+					contents: await profileImage(member.id)
 				},
 				{
-					name: `${member.user.id}.icon.png`,
+					name: `${member.id}.icon.png`,
 					contents: await ctx.util.canva.circle_cut(`${photo}?size=1024`)
 				}
 			]
