cdc2b72ea4f89c1bd1f2aade87eba24c8996058c
KingBeCats <=>
2024-08-16 16:03:30 -0700

    Testing Webhooks 2

--- src/commands/test.ts
+++ src/commands/test.ts
@@ -18,17 +18,13 @@ export default new Command(
 			},
 			body: JSON.stringify({
 				content: 'Este es un mensaje enviado mediante fetch.',
-				username: 'Nombre del Webhook',
-				avatar_url: 'https://url_del_avatar.png',
 				embeds: [
 					{
 						title: 'Título del Embed',
 						description: 'Descripción del Embed',
-						color: 16711680 // Color en formato decimal (0xFF0000 para rojo)
+						color: 16711680
 					}
-				],
-				components: [],
-				allowed_mentions: { parse: [] }
+				]
 			})
 		})
 			.then((r) => {
