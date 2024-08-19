8f1ff3338793eb1f7e18a8ef5743eb0d31af889f
KingBeCats <=>
2024-08-19 16:08:34 -0700

    Improved Console.log

--- src/events/interaction/modal.interaction.ts
+++ src/events/interaction/modal.interaction.ts
@@ -2,5 +2,5 @@ import { Event } from 'src/builders/event.builder';
 
 export default new Event('interactionCreate', async (i) => {
 	if (!i.isModalSubmitInteraction()) return;
-	console.log(i.data.components.raw);
+	console.log(i.data.components.raw.map((c) => c.components));
 });
